9ec7b15784fb83567e057c645d9d288c35070a8c
Legohead259 <legohead259@gmail.com>
2023-07-27 01:57:28 +0000

    Added example code to the DRV8825 node

--- src/drv8825_motor_controller/drv8825_motor_controller/drv8825.py
+++ src/drv8825_motor_controller/drv8825_motor_controller/drv8825.py
@@ -1,5 +1,95 @@
-def main():
-    print('Hi from drv8825_motor_controller.')
+import RPi.GPIO as GPIO
+import time
+import rclpy
+from rclpy.node import Node
+
+MotorDir = [
+    'forward',
+    'backward',
+]
+
+ControlMode = [
+    'hardward',
+    'softward',
+]
+
+class DRV8825(Node):
+    def __init__(self, dir_pin, step_pin, enable_pin, mode_pins):
+        self.dir_pin = dir_pin
+        self.step_pin = step_pin        
+        self.enable_pin = enable_pin
+        self.mode_pins = mode_pins
+        
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        GPIO.setup(self.dir_pin, GPIO.OUT)
+        GPIO.setup(self.step_pin, GPIO.OUT)
+        GPIO.setup(self.enable_pin, GPIO.OUT)
+        GPIO.setup(self.mode_pins, GPIO.OUT)
+        
+    def digital_write(self, pin, value):
+        GPIO.output(pin, value)
+        
+    def Stop(self):
+        self.digital_write(self.enable_pin, 0)
+    
+    def SetMicroStep(self, mode, stepformat):
+        """
+        (1) mode
+            'hardward' :    Use the switch on the module to control the microstep
+            'software' :    Use software to control microstep pin levels
+                Need to put the All switch to 0
+        (2) stepformat
+            ('fullstep', 'halfstep', '1/4step', '1/8step', '1/16step', '1/32step')
+        """
+        microstep = {'fullstep': (0, 0, 0),
+                     'halfstep': (1, 0, 0),
+                     '1/4step': (0, 1, 0),
+                     '1/8step': (1, 1, 0),
+                     '1/16step': (0, 0, 1),
+                     '1/32step': (1, 0, 1)}
+
+        print("Control mode:",mode)
+        if (mode == ControlMode[1]):
+            print("set pins")
+            self.digital_write(self.mode_pins, microstep[stepformat])
+        
+    def TurnStep(self, Dir, steps, stepdelay=0.005):
+        if (Dir == MotorDir[0]):
+            print("forward")
+            self.digital_write(self.enable_pin, 1)
+            self.digital_write(self.dir_pin, 0)
+        elif (Dir == MotorDir[1]):
+            print("backward")
+            self.digital_write(self.enable_pin, 1)
+            self.digital_write(self.dir_pin, 1)
+        else:
+            print("the dir must be : 'forward' or 'backward'")
+            self.digital_write(self.enable_pin, 0)
+            return
+
+        if (steps == 0):
+            return
+            
+        print("turn step:",steps)
+        for i in range(steps):
+            self.digital_write(self.step_pin, True)
+            time.sleep(stepdelay)
+            self.digital_write(self.step_pin, False)
+            time.sleep(stepdelay)
+            
+def main(args=None):
+    rclpy.init(args=args)
+
+    drv8825 = DRV8825(dir_pin=13, step_pin=19, enable_pin=12, mode_pins=(16, 17, 20))
+
+    rclpy.spin(drv8825)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    drv8825.destroy_node()
+    rclpy.shutdown()
 
 
 if __name__ == '__main__':
