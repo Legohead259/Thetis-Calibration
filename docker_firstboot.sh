6f31bf0f6e40d9aeef00c5f7fed2f5d83233e87f
Legohead259 <legohead259@gmail.com>
2023-07-27 20:39:40 +0000

    Added a new script to be run when the ROS docker container restarts. Changed the name of previous startup to First Boot since it only needs to be run at first boot

---
+++ docker_firstboot.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+# Setup ROS2 to be CLI accessible
+source /opt/ros/iron/setup.bash
+
+# Update the internal packages and upgrade to latest versions
+apt update && apt upgrade -y
+
+# Get PIP
+apt install python3-pip -y
+
+# Install RPi.GPIO
+pip3 install rpi.gpio
+
+# Configure git parameters
+git config --global user.name Legohead259
+git config --global user.email legohead259@gmail.com
+
+# VS Code Extensions
+# 1. Python
+# 2. Pylance
+# 3. AutoDocstring
--- docker_startup.sh
+++ docker_startup.sh
@@ -1,17 +1,4 @@
-#! /bin/bash
+#! bin/bash
 
-# Setup ROS2 to be CLI accessible
+# Source the ROS2 command
 source /opt/ros/iron/setup.bash
-
-# Update the internal packages and upgrade to latest versions
-apt update && apt upgrade -y
-
-# Get PIP
-apt install python3-pip -y
-
-# Install RPi.GPIO
-pip3 install rpi.gpio
-
-# Configure git parameters
-git config --global user.name Legohead259
-git config --global user.email legohead259@gmail.com
