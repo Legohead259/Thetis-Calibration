ecb9036815971658ce14f731ad2757fb9b90b40d
Legohead259 <legohead259@gmail.com>
2023-07-28 02:52:38 +0000

    FUlly implemented DRV8825 driver as a ROS node

--- src/drv8825_motor_controller/drv8825_motor_controller/drv8825.py
+++ src/drv8825_motor_controller/drv8825_motor_controller/drv8825.py
@@ -2,41 +2,84 @@ import RPi.GPIO as GPIO
 import time
 import rclpy
 from rclpy.node import Node
+from std_srvs.srv import Trigger
+from thetis_interfaces.srv import SetFloat64
 from enum import Enum
-import asyncio
+from math import log2, ceil
 
 class MicroSteps(Enum):
-    FULL_STEP           = 0
-    HALF_STEP           = 1
-    QUARTER_STEP        = 2
-    EIGHTH_STEP         = 3
-    SIXTEENTH_STEP      = 4
-    THIRTY_SECOND_STEP  = 5 
+    FULL_STEP           = 1
+    HALF_STEP           = 2
+    QUARTER_STEP        = 4
+    EIGHTH_STEP         = 8
+    SIXTEENTH_STEP      = 16
+    THIRTY_SECOND_STEP  = 32
+    
+class StepModes(Enum):
+    HARDWARE = 0
+    SOFTWARE = 1
+    
+class MotorDirection(Enum):
+    COUNTERCLOCKWISE = 0
+    CLOCKWISE = 1
 
 class DRV8825():
-    _mode : bool # The microstep control mode. 0 (false) for hardware, 1 (true) for software
-    _motor_dir : bool # The motor direction. 0 (false) for counter-clockwise, 1 (true) for clockwise
+    _is_enabled : bool = False  # If the motor controller is enabled or not
+    _mode : StepModes           # The microstep control mode. 0 (false) for hardware, 1 (true) for software
+    _motor_dir : MotorDirection # The motor direction. 0 (false) for counter-clockwise, 1 (true) for clockwise
+    _micro_steps : MicroSteps   # The number of microsteps currently being used
+    _steps_per_rev : int        # The number of steps required to make one full revolution
     
-    def __init__(self, dir_pin: int, step_pin: int, enable_pin: int, mode_pins: tuple):
-        self.dir_pin = dir_pin
-        self.step_pin = step_pin        
-        self.enable_pin = enable_pin
-        self.mode_pins = mode_pins
+    def __init__(self, dir_pin: int, step_pin: int, enable_pin: int, mode_pins: tuple, step_deg: float=1.8):
+        self._dir_pin = dir_pin
+        self._step_pin = step_pin        
+        self._enable_pin = enable_pin
+        self._mode_pins = mode_pins
+        self._step_deg = step_deg
+        self._steps_per_rev = 360.0 / step_deg
         
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
-        GPIO.setup(self.dir_pin, GPIO.OUT)
-        GPIO.setup(self.step_pin, GPIO.OUT)
-        GPIO.setup(self.enable_pin, GPIO.OUT)
-        GPIO.setup(self.mode_pins, GPIO.OUT)
+        GPIO.setup(self._dir_pin, GPIO.OUT)
+        GPIO.setup(self._step_pin, GPIO.OUT)
+        GPIO.setup(self._enable_pin, GPIO.OUT)
+        GPIO.setup(self._mode_pins, GPIO.OUT)
+        
+        self._step_pwm = GPIO.PWM(self._step_pin, 0) # Create PWM channel at 0 Hz
+        
+    def calculate_step_delay(self, velocity: float):
+        return (self._step_deg * self._micro_steps.value) / (2 * velocity)
     
-    def set_micro_step(self, mode: bool, step_format: MicroSteps):
+    def calculate_step_frequency(self, velocity: float):
+        return 1 / self.calculate_step_delay(velocity)
+    
+    def turn_steps(self, motor_dir: MotorDirection, steps: int, step_delay: float=0.0025):
+        self.enable()
+        self.set_motor_direction(motor_dir)
+        
+        if (steps == 0):
+            return
+            
+        for i in range(steps):
+            self.step(step_delay)
+        
+    def step(self, step_delay: float=0.0025):
+        self.digital_write(self._step_pin, True)
+        time.sleep(step_delay)
+        self.digital_write(self._step_pin, False)
+        time.sleep(step_delay)
+            
+    def set_motor_direction(self, motor_dir: MotorDirection):
+        self._motor_dir = motor_dir
+        self.digital_write(self._dir_pin, motor_dir)
+        
+    def set_micro_step(self, mode: StepModes, step_format: MicroSteps):
         """If the mode is set to software, this function will set the appropriate pins to
         set the microstep amount. Otherwise, the controller defaults to the DIP switches
         physically present on the hardware.
 
         Args:
-            mode (bool): the microstep set mode. 0 (false) for hardware, 1 (true) for software.
+            mode (StepMode): the microstep set mode. 0 (false) for hardware, 1 (true) for software.
             
             stepformat (MicroSteps): the step format used for the motor controller; found 
             within the MicroSteps enum
@@ -47,33 +90,31 @@ class DRV8825():
                      (1, 1, 0), # EIGHTH_STEP 
                      (0, 0, 1), # SIXTEENTH_STEP
                      (1, 0, 1)] # THIRTY_SECOND_STEP
-        if (mode):
-            self.digital_write(self.mode_pins, microstep[step_format])
+        self._mode = mode
+        self._micro_steps = step_format
         
-    async def turn_step(self, motor_dir: bool, steps: int, step_delay: float=0.0025):
-        if (not motor_dir):
-            self.digital_write(self.enable_pin, 1)
-            self.digital_write(self.dir_pin, 0)
-        else:
-            self.digital_write(self.enable_pin, 1)
-            self.digital_write(self.dir_pin, 1)
-
-        if (steps == 0):
-            return
+        if (mode.value):
+            self.digital_write(self._mode_pins, microstep[ceil(log2(step_format.value))])
             
-        for i in range(steps):
-            self.digital_write(self.step_pin, True)
-            await asyncio.sleep(step_delay)
-            self.digital_write(self.step_pin, False)
-            await asyncio.sleep(step_delay)
+    def enable(self):
+        self._is_enabled = True
+        self.digital_write(self._enable_pin, True)
             
-    def stop(self):
-        self.digital_write(self.enable_pin, 0)
+    def disable(self):
+        self._is_enabled = False
+        self.digital_write(self._enable_pin, False)
+        
+    def cleanup_gpio(self):
+        self.stop()
+        GPIO.cleanup()
             
     def digital_write(self, pin, value):
         GPIO.output(pin, value)
+        
             
 class DRV8825Node(DRV8825, Node):
+    target_velocity : float = 360
+    
     def __init__(self):
         Node.__init__(self, 'DRV8825Node')
 
@@ -81,36 +122,70 @@ class DRV8825Node(DRV8825, Node):
         self.declare_parameter('step_pin', 19)
         self.declare_parameter('enable_pin', 12)
         self.declare_parameter('mode_pins', (16, 17, 20))
+        self.declare_parameter('step_mode', StepModes.SOFTWARE.value)
         
         self.dir_pin = self.get_parameter('dir_pin').get_parameter_value().integer_value
         self.step_pin = self.get_parameter('step_pin').get_parameter_value().integer_value
         self.enable_pin = self.get_parameter('enable_pin').get_parameter_value().integer_value
         self.mode_pins = self.get_parameter('mode_pins').get_parameter_value().integer_array_value
         self.mode_pins = list(self.mode_pins)
+        self.step_mode = self.get_parameter('step_mode').get_parameter_value().bool_value
 		
         self.get_logger().info(f"Using direction pin: {self.dir_pin}")
         self.get_logger().info(f"Using step pin: {self.step_pin}")
         self.get_logger().info(f"Using enable pin: {self.enable_pin}")
         self.get_logger().info(f"Using mode pins: {self.mode_pins}")
+        self.get_logger().info(f"Using step mode: {self.step_mode}")
         
         DRV8825.__init__(self,
                          dir_pin=self.dir_pin, 
                          step_pin=self.step_pin, 
                          enable_pin=self.enable_pin, 
                          mode_pins=self.mode_pins)
+        
+        # Create services
+        self.start_service = self.create_service(Trigger, 'start_motor', self.enable_motor_callback)
+        self.stop_service = self.create_service(Trigger, 'stop_motor', self.disable_motor_callback)
+        self.set_speed_service = self.create_service(SetFloat64, 'set_motor_speed', self.set_speed_callback)
+        self.get_logger().info("Initialized services")
+        
+    def motor_loop(self):
+        pass
+                
+    def enable_motor_callback(self, request, response):
+        if not self._is_enabled:
+            self._is_enabled = True
+            self._step_pwm.start(50)
+            response.success = True
+            response.message = "Motor started"
+        else:
+            response.success = False
+            response.message = "Motor is already running"
+        return response
+
+    def disable_motor_callback(self, request, response):
+        if self._is_enabled:
+            self._is_enabled = False
+            self._step_pwm.stop()
+            response.success = True
+            response.message = "Motor stopped"
+        else:
+            response.success = False
+            response.message = "Motor is not running"
+        return response
+
+    def set_speed_callback(self, request, response):
+        self._step_pwm.ChangeFrequency(self.calculate_step_frequency(request.data))
+        response.success = True
+        response.message = f"Motor speed set to {request.data} deg/sec"
+        return response
+                
             
 def main(args=None):
     rclpy.init(args=args)
-
-    drv8825 = DRV8825Node()
-    
-    drv8825.get_logger().info("Starting stepper motor movement...")
-    asyncio.run(drv8825.turn_step(motor_dir=True, steps=100))
-
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    # drv8825.destroy_node()
+    stepper_node = DRV8825Node()
+    stepper_node.set_micro_step(StepModes.SOFTWARE, MicroSteps.FULL_STEP)
+    rclpy.spin(stepper_node)
     rclpy.shutdown()
 
 
