8c5c30d66cb1d0d8e44857165ff2886a4d06c6a3
Legohead259 <legohead259@gmail.com>
2023-08-10 19:39:58 +0000

    Added examples for the new I2C components Updated first_run script to install support

--- docker_firstboot.sh
+++ docker_firstboot.sh
@@ -21,3 +21,19 @@ git config --global user.email legohead259@gmail.com
 # 2. Pylance
 # 3. AutoDocstring
 # 4. CMake Tools
+
+# Install CircuitPython support
+pip3 install adafruit-blinka 
+pip3 install adafruit-circuitpython-mlx90393 
+# pip3 install adafruit-circuitpython-ssd1327 # CircuitPython for SSD1327 currently not working
+pip3 install adafruit-circuitpython-seesaw
+
+
+# Install luma.oled for SSD1327
+apt install python3-dev python3-pip python3-numpy libfreetype6-dev libjpeg-dev build-essential
+apt install libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libportmidi-dev
+cd /home/
+git clone https://github.com/rm-hull/luma.examples.git
+cd luma.examples/
+sudo -H pip install -e .
+cd /home/
