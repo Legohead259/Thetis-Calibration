4a3a24d83a329c03814ba88bf32bb187bbcb3464
Braidan <legohead259@gmail.com>
2023-12-05 10:11:08 -0500

    Update ws_initialize.sh
    Migrated ROS2 initialization to a dedicated repo

--- ws_initialize.sh
+++ ws_initialize.sh
@@ -1,33 +1,7 @@
 #! /bin/bash
 
-# Update the internal packages and upgrade to latest versions
-sudo apt update && apt upgrade -y
-
-# Install SSH capability
-sudo apt install openssh-server -y
-
-# Install and configure git
-sudo apt install git -y
-git config --global user.name Legohead259
-git config --global user.email legohead259@gmail.com
-
-# Install ROS2 (https://docs.ros.org/en/iron/Installation/Ubuntu-Install-Debians.html)
-sudo apt install software-properties-common -y
-sudo add-apt-repository universe -y
-sudo apt update && sudo apt install curl -y
-sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
-sudo apt update && sudo apt install ros-dev-tools -y
-sudo apt install ros-iron-desktop -y
-
-# Setup ROS2 to be CLI accessible
-source /opt/ros/iron/setup.bash
-
-# Ensure all future terminals can access the `ros2` CLI
-echo "source /opt/ros/iron/setup.bash" >> ~/.bashrc
-
-# Get PIP
-sudo apt install python3-pip -y
+# Update system and install required libraries and applications (ROS2)
+wget -O - https://raw.githubusercontent.com/Legohead259/Scripting/main/ROS2/ws-initialize-linux.sh | bash
 
 # Install RPi.GPIO (https://askubuntu.com/questions/1230947/gpio-for-raspberry-pi-gpio-group)
 pip3 install rpi.gpio
@@ -43,13 +17,6 @@ pip3 install PyQT5
 
 # Install xIMU3 support
 pip3 install ximu3
-
-# VS Code Extensions
-code --install-extension ms-python.python                       # Python/Pylance
-code --install-extension njpwerner.autodocstring                # AutoDocstring
-code --install-extension ms-vscode.cpptools                     # C++ Tools
-code --install-extension ms-vscode.cmake-tools                  # CMake Tools
-code --install-extension streetsidesoftware.code-spell-checker  # Code Spell Checker
 
 # Restart system
 sudo reboot
